dae56f753c58d473673ea6299c8167bf69beb664
Juhas <juhas1998@gmail.com>
2024-09-11 16:52:16 +0600

    removed unwanted files

--- BloodManagement/Data/Interface/IDonationRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using BloodManagement.Models.Domain;
-
-namespace BloodManagement.Data.Interface
-{
-    public interface IDonationRepository: IBaseRepository<Donation>
-    {
-        Task<List<Donation>> SearchByName(string name);
-    }
-}
--- BloodManagement/Data/Interface/IDonorRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using BloodManagement.Models.Domain;
-
-namespace BloodManagement.Data.Interface
-{
-    public interface IDonorRepository: IBaseRepository<Donor>
-    {
-        Task<List<Donor>> SearchByName(string name);
-    }
-}
